666c77292b732a1cc2d447e7d51ac7505ed07bd9
Nukol <lengbowen4@gmail.com>
2026-02-10 08:42:59 -0600

    Initial commit

---
+++ blogscript.js
@@ -0,0 +1,4 @@
+// basic idea, read every single json file in the blogs folder and then sort them by date
+// after sorting by date, use a for loop to add new elements with the ids of "#sec" + (number in the array)
+// List of possible allowed sorting criteria: sort by relevancy, sort by recency, and sort by authors
+// maybe allow a search bar of some kind as well. A general search, search by title & subject, and search by author.
---
+++ script.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function() {
+    var elem = jQuery(".nav").find("span");
+    var secs = [];
+    let i = 1;
+    while (jQuery("#sec" + i).length) {
+        secs.push(jQuery("#sec" + i));
+        i++;
+    }
+    jQuery("#dropdown-content").hover(function() {
+        elem.css("background-color", "#ddd");
+    }, function() {
+        elem.css("background-color", "#889199");
+    })
+    elem.hover(function() {
+        elem.css("background-color", "#ddd");
+    }, function() {
+        elem.css("background-color", "#889199");
+    });
+    jQuery(window).scroll(function() {
+        var height = jQuery(window).scrollTop() + 100;
+        for (let i = 0; i < secs.length; i++) {
+            if (height > secs[i].scrollTop() + 50) {
+                secs[i].animate({
+                    opacity: "1"
+                }, {
+                    duration: "5000"
+                });
+            }
+        }
+    });
+})
