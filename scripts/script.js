f490007a45e959914c34cbcf5a350a2bc3e1c59e
Nukol <lengbowen4@gmail.com>
2026-02-19 19:37:53 -0600

    Changed backgrounds of team page.

--- scripts/script.js
+++ scripts/script.js
@@ -32,6 +32,45 @@ jQuery(document).ready(function() {
 
 i = 0
 jQuery(document).ready(function () {
+    let change = false;
+    const date = new Date();
+    const m = date.getMonth();
+    const d = date.getDate();
+    const dayofweek = date.getDay();
+    let event_img = "assets/team-bg.png";
+    if (m == 1 && d == 14) {
+        event_img = "assets/valentines-bg.png"; // todo, whenever the image gets implemented, change png to jpg if needed.
+        // change = true;
+    }
+    if (m == 2 && (d == 17 || d == 18)) {
+        event_img = "assets/patricks-day-bg.png"; // todo, whenever the image gets implemented, change png to jpg if needed.
+        // change = true;
+    }
+    if (m == 3 && d == 1) {
+        if (d == 1) {
+            event_img = "assets/april-fools-bg.gif";
+            change = true;
+        } else if (d >= 2) { // todo, make this proper
+            event_img = "assets/easter-bg.png"; // todo, whenever the image gets implemented, change png to jpg if needed.
+            // change = true;
+        }
+    }
+    if (m == 9 && d == 30) {
+        event_img = "assets/halloween-bg.png"; // todo, whenever the image gets implemented, change png to jpg if needed.
+        // change = true;
+    }
+    if (m == 11 && d >= 20) { // todo, make this proper
+        event_img = "assets/christmas-bg.png"; // todo, whenever the image gets implemented, change png to jpg if needed.
+        // change = true;
+    }
+
+    if (change) {
+        jQuery("#team-body").css(
+            "background-image",
+            "url(" + event_img + ")"
+        )
+    }
+
     const images = [
         "assets/bg1.gif",
         "assets/bg2.jpg",
@@ -44,23 +83,6 @@ jQuery(document).ready(function () {
             "background-image",
             "url(" + images[index] + ")"
         );
-
-        let change = false;
-        const date = new Date();
-        const m = date.getMonth();
-        const d = date.getDate();
-        let event_img = "assets/team-bg.png";
-        if (m == 3 && d == 1) {
-            event_img = "assets/april-fools-bg.gif";
-            change = true;
-        }
-
-        if (change) {
-            jQuery("#team-body").css(
-                "background-image",
-                "url(" + event_img + ")"
-            )
-        }
 
         index = (index + 1) % images.length;
         return images[index].substring(images.length - 3) == "gif" ? 20000 : 5000;
